7436ef1e42a53c20264ed0ef8a97db56082b0a94
Lena Kurenkova <light.reindeer@gmail.com>
2019-11-14 00:33:02 +0200

    Add cart, functional, ajax requsts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './scss/main.scss';
+//Functional of cart
 var cartFunctional = function() {
     var cart = [];
     var allItems = 0;
@@ -96,6 +97,7 @@ var cartFunctional = function() {
     };
 
     obj.cartContent = function() {
+    	console.log(cart);
         return cart;
     };
 
@@ -107,16 +109,11 @@ function updateButtonCart(button) {
     button.text("Cart (" + cartFunctional.totalAmount() + ")");
 }
 
-//Handling events
+
+
 $(document).ready(function() {
 
-    //Add item to cart
-    $(".buy_button").click(function() {
-        cartFunctional.addItem(
-            $(this).parent().find(".card-title").text(),
-            parseFloat($(this).parent().find(".card-price").text()))
-        updateButtonCart($(".cart_button"));
-    });
+    //Handling cart events
 
     //Distplay cart
     $(".cart_button").click(function() {
@@ -178,5 +175,220 @@ $(document).ready(function() {
         $(".total-price").text(cartFunctional.totalPrice());
         updateButtonCart($(".cart_button"));
 
+    });
+
+    $.ajax({
+        url: 'https://nit.tron.net.ua/api/product/list',
+        type: 'GET',
+        success: function(content) {
+            var s = "";
+            for (var i of content) {
+                s += '<div class="col-lg-3 col-md-6 my-3">' +
+                    '<div class="card h-100" data-id=' + i.id + ' ">' +
+                    '<div class="image-wrapper card-image-top" style="background-image:url(' + i.image_url + ')"></div>' +
+                    '<div class="card-body d-flex flex-column">' +
+                    '<div class="card-title">' + i.name + '</div>' +
+                    '<div class="mt-auto">';
+                if (i.special_price == null) { s += '<div class="card-text mb-2 card-price">' + i.price + '$ </div>' } else {
+                    s += '<div class="card-text mb-2 card-price"><span class="old-price">' + i.price + '$   </span>' +
+                        '<span class="special-price">' + i.special_price + '</span></div>'
+                }
+                s +=
+                    '<button type="button" class="btn btn-dark info_button" data-toggle="modal" data-target="#product_info"> Info </button>' +
+                    '<button class="btn btn-dark mx-1 buy_button">Buy</button>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>'
+            }
+            $('.product-board').html(s);
+        },
+        error: function() {
+            alert('Error while loading data!');
+        },
+    });
+
+    $.ajax({
+        url: 'https://nit.tron.net.ua/api/category/list',
+        type: 'GET',
+        success: function(content) {
+            var s = "<a class=\"dropdown-item light-text all-products\" href=\"#\">All products</a>";
+            for (var i of content) {
+                s += "<a class=\"dropdown-item category-name light-text\" data-id=\"" + i.id + "\"href=\"#\">" + i.name + "</a>";
+            }
+            $('div.category-list').html(s);
+        },
+        error: function() {
+            alert('Error while loading data!');
+        },
+    });
+
+    $(".dropdown").on("click", ".dropdown-item.all-products", function() {
+        $('h1.categoty-name').text("All products");
+        $('.categoty-description').text("");
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/product/list',
+            type: 'GET',
+            success: function(content) {
+                var s = "";
+                for (var i of content) {
+                    s += '<div class="col-lg-3 col-md-6 my-3">' +
+                        '<div class="card h-100" data-id=' + i.id + ' ">' +
+                        '<div class="image-wrapper card-image-top" style="background-image:url(' + i.image_url + ')"></div>' +
+                        '<div class="card-body d-flex flex-column">' +
+                        '<div class="card-title">' + i.name + '</div>' +
+                        '<div class="mt-auto">';
+                    if (i.special_price == null) { s += '<div class="card-text mb-2 card-price">' + i.price + '$ </div>' } else {
+                        s += '<div class="card-text mb-2 card-price"><span class="old-price">' + i.price + '$   </span>' +
+                            '<span class="special-price">' + i.special_price + '</span></div>'
+                    }
+                    s +=
+                        '<button type="button" class="btn btn-dark info_button" data-toggle="modal" data-target="#product_info"> Info </button>' +
+                        '<button class="btn btn-dark mx-1 buy_button">Buy</button>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                }
+                $('.product-board').html(s);
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+    });
+
+    $(".dropdown").on("click", ".dropdown-item.category-name", function() {
+        var id = $(this).data().id;
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/category/' + id,
+            type: 'GET',
+            success: function(content) {
+                $('h1.categoty-name').text(content.name);
+                $('.categoty-description').text(content.description);
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/product/list/category/' + id,
+            type: 'GET',
+            success: function(content) {
+                var s = "";
+                for (var i of content) {
+                    s += '<div class="col-lg-3 col-md-6 my-3">' +
+                        '<div class="card h-100" data-id=' + i.id + ' ">' +
+                        '<div class="image-wrapper card-image-top" style="background-image:url(' + i.image_url + ')"></div>' +
+                        '<div class="card-body d-flex flex-column">' +
+                        '<div class="card-title">' + i.name + '</div>' +
+                        '<div class="mt-auto">';
+                    if (i.special_price == null) { s += '<div class="card-text mb-2 card-price">' + i.price + '$ </div>' } else {
+                        s += '<div class="card-text mb-2 card-price"><span class="old-price">' + i.price + '$   </span>' +
+                            '<span class="special-price">' + i.special_price + '</span></div>'
+                    }
+                    s +=
+                        '<button type="button" class="btn btn-dark info_button" data-toggle="modal" data-target="#product_info"> Info </button>' +
+                        '<button class="btn btn-dark mx-1 buy_button">Buy</button>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                }
+                $('.product-board').html(s);
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+    });
+
+    $(".product-board").on("click", "button.info_button", function() {
+        var id = $(this).parents('.card').data().id;
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/product/' + id,
+            type: 'GET',
+            success: function(content) {
+                $('.modal-body .product-name').text(content.name);
+                if (content.special_price == null) {
+                    $('.modal-body .price').text(content.price);
+                } else {
+                    $('.modal-body .price').addClass('.old-price');
+                    $('.modal-body .special-price').text(content.special_price);
+                }
+                $('.info_modal.image-wrapper').css("background-image", "url(" + content.image_url + ")");
+                $('.modal-body .product-description').text(content.description);
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+    });
+    //Add item to cart
+    $(document).on("click", ".buy_button", function() {
+        var id = $(this).parents('.card').data().id;
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/product/' + id,
+            type: 'GET',
+            success: function(content) {
+                console.log(content);
+                if (content.special_price == null) {
+                    cartFunctional.addItem(content.name, parseFloat(content.price));
+                } else {
+                    cartFunctional.addItem(content.name, parseFloat(content.special_price));
+                }
+                updateButtonCart($(".cart_button"));
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+    });
+
+    $(document).on("click", "#buy_button", function() {
+        var id = $(this).parents('.card').data().id;
+        $.ajax({
+            url: 'https://nit.tron.net.ua/api/product/' + id,
+            type: 'GET',
+            success: function(content) {
+                console.log(content);
+                if (content.special_price == null) {
+                    cartFunctional.addItem(content.name, parseFloat(content.price));
+                } else {
+                    cartFunctional.addItem(content.name, parseFloat(content.special_price));
+                }
+                updateButtonCart($(".cart_button"));
+            },
+            error: function() {
+                alert('Error while loading data!');
+            },
+        });
+    });
+
+    $(document).on("click", "#submit", function() {
+        var dataString = "token=kHPdQX3-vwtFW9o4fPXB";
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var phone = $("input#phone").val();
+        var products = "";
+
+        dataString += '&name=' + name + '&phone=' + phone + '&email=' + email + '&' + products;
+
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: ,
+        //     data: dataString,
+        //     success: function() {
+        //         $('#contact_form').html("<div id='message'></div>");
+        //         $('#message').html("<h2>Contact Form Submitted!</h2>")
+        //             .append("<p>We will be in touch soon.</p>")
+        //             .hide()
+        //             .fadeIn(1500, function() {
+        //                 $('#message').append("<img id='checkmark' src='images/check.png' />");
+        //             });
+        //     }
+        // });
     });
 });
